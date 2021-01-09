7a0d88dc93fef7702b84ef7560493350186e0ece
XuHomeDesktop <jixu6@163.com>
2021-01-09 15:06:46 -0600

    update

--- XuGetESF.m
+++ XuGetESF.m
@@ -1,5 +1,8 @@
 function [esf, corr ] =  XuGetESF(edgedata,resampling_interval)
 l_s_f = XuGetLineSpreadFunction(edgedata); 
 l_s_f_resample = XuSpreadFunctionResample(l_s_f,resampling_interval);
+
+l_s_f_resample(isnan(l_s_f_resample)) = 0;
+
 esf = cumsum(l_s_f_resample(2,:))*resampling_interval;
 corr = l_s_f_resample(1,:);
---
+++ XuLogFactorial.m
@@ -0,0 +1,3 @@
+function output = XuLogFactorial(n)
+output = 1/2*log(2*pi*n) + n*log(n) -n;
+end
